93e0e266bee6dc33bcac7541f3b5a18e74115624
aleciamaybee <104103015+aleciamaybee@users.noreply.github.com>
2022-06-29 16:29:18 -0400

    Updated JS to display daily data - logged result

--- src/index.js
+++ src/index.js
@@ -25,7 +25,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -57,6 +58,7 @@ function getForecast(coordinates) {
   let apiKey = "fb3734fada8c447e20db0e68e4f01b87";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
